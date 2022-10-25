86a6f97da877c3925540abc184a732a3e3c15531
Oksana Polaikova <it.oksana.poliakova@gmail.com>
2022-10-25 18:42:01 +0300

    Initial Commit

---
+++ MapsAppSwiftUI/MapsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsAppSwiftUIApp.swift
+//  MapsAppSwiftUI
+//
+//  Created by Oksana on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
